38e33e3fa486721e3e8a648bd38558d4b39305eb
Harsh <harshh.tsx@gmail.com>
2025-03-16 16:50:47 +0530

    Loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import LoaderAnimation from "../Components/Loader";
 
 const Home = () => {
-  return (
-    <div>
-      <LoaderAnimation />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
