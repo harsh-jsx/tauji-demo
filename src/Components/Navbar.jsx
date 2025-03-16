21d26920bd0cc091e5549c4312bca8354a7879b7
maddemon798 <hardiktaneja838@gmail.com>
2025-03-16 18:31:35 +0530

    added navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Logo from "../assets/Logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex justify-between items-center w-full px-16 h-[10vh] z-[10]">
+      <div className="brand-section flex flex-row gap-2">
+        <h1 className="text-white bold font">Rajani</h1>
+        <img src={Logo} alt="logo" className="w-24" />
+        <h1>Properties</h1>
+      </div>
+      <div className="menu-section flex flex-row gap-3">
+        <div className="p-5 bg-[#EB3535]">Menu</div>
+        <div className="flex flex-row gap-3">
+          <i class="fa-brands fa-facebook-f"></i>
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-linkedin-in"></i>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Navbar from "../Components/Navbar";
+import BGVIDEO from "../assets/BGVIDEO.mp4";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="w-full min-h-[100vh]">
+        <Navbar />
+        <video
+          src={BGVIDEO}
+          className="absolute top-0 left-0 opacity-[.3] h-[100vh] w-full object-cover z-[-1]"
+          loop
+          autoPlay
+          muted
+        ></video>
+      </div>
+    </>
+  );
 };
 
 export default Home;
